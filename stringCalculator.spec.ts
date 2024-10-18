7c252401c63ead75d2373ea7abfe3aee70cb05a1
Shreshta126 <ee18btech11041@iith.ac.in>
2024-10-18 16:40:06 +0530

    4 Test Case with delimiter

--- stringCalculator.spec.ts
+++ stringCalculator.spec.ts
@@ -11,7 +11,15 @@ describe('StringCalculator', () => {
         expect(stringCalculator.add("")).toBe(0);
     });
 
-    test('should return the sum of numbers in string without delimiters', () => {
+    test('should return the sum of numbers in string with comma seperated numbers', () => {
+        expect(stringCalculator.add("1,2,3,4")).toBe(10);
+    });
+
+    test('should return the sum of numbers in string with new line character', () => {
         expect(stringCalculator.add("1\n2,3\n4\n5,6")).toBe(21);
+    });
+
+    test('should return the sum of numbers in string with delimiters', () => {
+        expect(stringCalculator.add("//[;]\n1;2;3;4;5;6")).toBe(21);
     });
 });
--- stringCalculator.ts
+++ stringCalculator.ts
@@ -4,8 +4,23 @@ class StringCalculator {
         if(numbers == ""){
             return 0;
         }
+        let numString = "";
+        let delimiter:any;
+        if(numbers.substring(0,2)=="//")
+        {
+            const split = numbers.split('\n', 2);
+            numString = split[1];
+            delimiter = new RegExp(split[0].substring(2));
+        }
+        let splitNumbers:any; 
 
-        let splitNumbers = numbers.split(/\n|,/); 
+        if(delimiter)
+        {
+            splitNumbers = numString.split(delimiter);
+        }else{
+            splitNumbers = numbers.split(/\n|,/); 
+        }
+        
         let totalSum = 0;
         for(const num of splitNumbers){
             totalSum+= Number(num);
@@ -13,5 +28,4 @@ class StringCalculator {
         return totalSum;
     }
 }
-
 export default StringCalculator;
